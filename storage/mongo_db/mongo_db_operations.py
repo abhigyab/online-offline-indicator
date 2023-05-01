69f78b0f4ab99839410288d71d56ec694dfdcb36
Abhigya Bharadwaj <abhigyab@microsoft.com>
2023-05-01 22:03:22 +0530

    add structure for mongo db client

---
+++ storage/mongo_db/mongo_db_operations.py
@@ -0,0 +1,18 @@
+"""
+Module to handle all the operations related to the MongoDB database.
+"""
+
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
+from storage.database_operations_base import DatabaseOperationsBase
+
+class MongoDBOperations(DatabaseOperationsBase):
+    """
+    Class to handle all the operations related to the MongoDB database.
+    """
+
+    def __init__():
+        pass
+
+if __name__ == '__main__':
+    print("abhigya")
